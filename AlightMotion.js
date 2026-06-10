60264846ddfe225bc0c60baa760767f5bc33654e
coneglitch <khanhtranvan392@gmail.com>
2026-06-10 21:05:26 +0700

    Update AlightMotion.js

--- AlightMotion.js
+++ AlightMotion.js
@@ -1,19 +1,24 @@
+const now = new Date();
+const currentTs = now.getTime();
+const expireTs = currentTs + (5 * 365 * 24 * 60 * 60 * 1000); // 5 years from now
+const fakeOrderId = "30000" + Math.floor(Math.random() * 9000000000 + 1000000000);
+
 const objc = {
     result: {
         result: "success",
-        msTime: 1704758400000,
+        msTime: currentTs,
         accountCreatedMillis: null,
         licenses: [{
             benefits: ["RemoveWatermark", "MemberEffects", "ProjectPackageSharing", "FutureMemberFeatures", "AdvancedEasing", "CameraObjects", "LayerParenting", "CloudStorageLowTier"],
             type: "subscription",
             store: "apple_app_store",
             autoRenewing: true,
-            orderNumber: "300001752007005",
+            orderNumber: fakeOrderId,
             productId: "alightcreative.motion.1y_t60_1w",
             period: "1y",
             label: null,
             details: null,
-            expires: 32662137600000,
+            expires: expireTs,
             valid: true,
             linkStatus: "linked-current"
         }],
